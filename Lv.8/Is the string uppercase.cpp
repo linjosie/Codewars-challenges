9b396a28be947045877f54212e58e85421b7db9a
Josephine Lin <38266110+linjosie@users.noreply.github.com>
2020-07-14 14:51:32 -0400

    Create Is the string uppercase.cpp
    Create a function is_uppercase() to see whether the string is ALL CAPS

---
+++ Lv.8/Is the string uppercase.cpp
@@ -0,0 +1,37 @@
+bool is_uppercase(const std::string &s) {
+	int len = s.length();
+	bool ans = true;
+	for(int i = 0 ; i < len; i++){
+		if(s[i]>=97 && s[i]<=122){
+			ans = false;
+		}
+	}
+	return ans;
+}
+
+///////other solutions
+
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool is_uppercase(const string &s)
+{
+    string answer = s;
+    transform(answer.begin(), answer.end(), answer.begin(), ::toupper);
+    return s == answer;
+}
+
+//////
+
+
+#include <string>
+
+bool is_uppercase(const std::string &s) {
+  for(char c : s)
+  {
+    if(!isupper(c) && isalpha(c))return false;
+  }
+  return true;
+}
