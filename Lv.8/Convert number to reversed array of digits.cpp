2a2ce261045a2b26fcf2a3dde4540977ca6ff63f
Josephine Lin <38266110+linjosie@users.noreply.github.com>
2020-07-24 15:41:44 -0400

    Create Convert number to reversed array of digits.cpp
    Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
    
    Example:
    348597 => [7,9,5,8,4,3]

---
+++ Lv.8/Convert number to reversed array of digits.cpp
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <vector>
+std::vector<int> digitize(unsigned long n) 
+{        
+  std::vector<int> vec={};
+  int digit = log10(n)+1;
+  for(int i=0;i<digit;i++){
+    vec.push_back(n%10);
+    n/=10;
+  }
+  return vec;
+  
+}
+
+//// other solutions
+
+std::vector<int> digitize(unsigned long n) 
+{
+    std::vector <int> num;
+    while(n!=0){
+        num.push_back(n%10);//fetch the LSB of the number
+        n = n / 10;//right shift the number
+    }
+    return num;
+}
+
+
+////
+std::vector<int> digitize(unsigned long long n) 
+{        
+  std::vector<int> digits;
+  for(;n>0;digits.push_back(n%10),n/=10);
+  return digits;
+}
